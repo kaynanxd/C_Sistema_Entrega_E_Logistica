1aaa8f84cc9d2af54bc2cfb50920b11f0d1243e8
kaynan <78617015+kaynanxd@users.noreply.github.com>
2025-01-29 17:47:17 -0300

    funcao alterar adicionada kay.c

--- kay.c
+++ kay.c
@@ -1,16 +1,14 @@
-#include<stdio.h>
-#include<stdlib.h>
+#include <stdio.h>
+#include <stdlib.h>
 #include <string.h>
 
-//declarar estrutura
-typedef struct{
-
+// Declarar estrutura
+typedef struct {
     int idfuncionario;
     char *nomefuncionario;
 } estruturafuncionario;
 
-//aloca memoria pra a lista da estrutura
-
+// Alocar memória para a lista da estrutura
 estruturafuncionario* alocarEstruturaFuncionario(int numdefuncionarios) {
     estruturafuncionario *funcionario = malloc(numdefuncionarios * sizeof(estruturafuncionario));
     if (funcionario == NULL) {
@@ -20,9 +18,8 @@ estruturafuncionario* alocarEstruturaFuncionario(int numdefuncionarios) {
     return funcionario;
 }
 
-//funcao para adicionarfuncionario
-void adicionarfuncionario(estruturafuncionario **funcionario, int *numdefuncionarios){
-    
+// Função para adicionar funcionario
+void adicionarfuncionario(estruturafuncionario **funcionario, int *numdefuncionarios) {
     for (int i = 0; i < *numdefuncionarios; i++) {
         (*funcionario)[i].nomefuncionario = malloc(30 * sizeof(char));
 
@@ -35,28 +32,26 @@ void adicionarfuncionario(estruturafuncionario **funcionario, int *numdefunciona
         getchar();
     }
 }
-    
-//funcao para salvar funcionario dentro do arquivo
-void salvarfuncionario(estruturafuncionario *funcionario,int numdefuncionarios ) {
+
+// Função para salvar funcionario dentro do arquivo
+void salvarfuncionario(estruturafuncionario *funcionario, int numdefuncionarios) {
     FILE* arq_funcionarios = fopen("arq_funcionarios.txt", "w");
     if (arq_funcionarios == NULL) {
         printf("Erro ao encontrar o arquivo");
         return;
     }
-    
+
     for (int i = 0; i < numdefuncionarios; i++) {
         fprintf(arq_funcionarios, "===== Informações dos funcionarios (%d) =====\n", i + 1);
         fprintf(arq_funcionarios, "Nome: %s\n", funcionario[i].nomefuncionario);
         fprintf(arq_funcionarios, "ID: %d\n", funcionario[i].idfuncionario);
-
     }
 
     fclose(arq_funcionarios);
-    printf("Os dados dos veículos cadastrados foram salvos com sucesso.\n");
+    printf("Os dados dos funcionarios cadastrados foram salvos com sucesso.\n");
 }
 
-//funcao para contar a quantidade de funcionarios no arquivo
-
+// Função para contar a quantidade de funcionarios no arquivo
 int contarfuncionariosNoArquivo() {
     FILE *arq_funcionarios = fopen("arq_funcionarios.txt", "r");
     if (arq_funcionarios == NULL) {
@@ -77,8 +72,7 @@ int contarfuncionariosNoArquivo() {
     return count;
 }
 
-
-//funcao para exibir todos funcionarios
+// Função para exibir todos funcionarios
 void visualizarfuncionario(estruturafuncionario *funcionario, int numdefuncionarios) {
     FILE *arq_funcionarios = fopen("arq_funcionarios.txt", "r");
     if (arq_funcionarios == NULL) {
@@ -99,7 +93,7 @@ void visualizarfuncionario(estruturafuncionario *funcionario, int numdefuncionar
                 printf("Nome:%s", nome);
             }
 
-             if (fgets(buffer, sizeof(buffer), arq_funcionarios)) {
+            if (fgets(buffer, sizeof(buffer), arq_funcionarios)) {
                 char *id = strstr(buffer, "ID: ");
                 id += strlen("ID: ");
                 printf("ID:%s", id);
@@ -113,57 +107,94 @@ void visualizarfuncionario(estruturafuncionario *funcionario, int numdefuncionar
     fclose(arq_funcionarios);
 }
 
-//funcao para alterarFuncionario
+// Função para alterar funcionario
 void alterarFuncionario(estruturafuncionario *funcionario, int numdefuncionarios) {
-    int id, encontrado = 0;
-    printf("Informe o ID do funcionário que deseja alterar: ");
-    scanf("%d", &id);
-    getchar();
+    int idParaAlterar;
+    char novoNome[30];
+    int novoId;
+
+    printf("Informe o ID do funcionario que deseja alterar: ");
+    scanf("%d", &idParaAlterar);
+    getchar(); // Limpar o buffer do teclado
 
     for (int i = 0; i < numdefuncionarios; i++) {
-        if (funcionario[i].idfuncionario == id) {
-            encontrado = 1;
-            printf("Informe o novo nome do funcionário: ");
-            fgets(funcionario[i].nomefuncionario, 30, stdin);
-            funcionario[i].nomefuncionario[strcspn(funcionario[i].nomefuncionario, "\n")] = 0;
+        if (funcionario[i].idfuncionario == idParaAlterar) {
+            printf("Informe o novo nome do funcionario: ");
+            fgets(novoNome, 30, stdin);
+            novoNome[strcspn(novoNome, "\n")] = 0;
 
-            printf("Informe o novo ID do funcionário: ");
-            scanf("%d", &funcionario[i].idfuncionario);
-            getchar();
-            break;
+            printf("Informe o novo ID do funcionario: ");
+            scanf("%d", &novoId);
+            getchar(); // Limpar o buffer do teclado
+
+            // Liberar a memória alocada para o nome anterior
+            free(funcionario[i].nomefuncionario);
+
+            // Alocar memória para o novo nome
+            funcionario[i].nomefuncionario = malloc(30 * sizeof(char));
+            strcpy(funcionario[i].nomefuncionario, novoNome);
+
+            funcionario[i].idfuncionario = novoId;
+
+            printf("Dados do funcionario alterados com sucesso.\n");
+            return;
         }
     }
 
-    if (!encontrado) {
-        printf("Funcionário com ID %d não encontrado.\n", id);
-    } else {
-        salvarfuncionario(funcionario, numdefuncionarios);
-        printf("Funcionário alterado com sucesso.\n");
-    }
+    printf("Funcionario com ID %d não encontrado.\n", idParaAlterar);
 }
 
-//funcao para deletarFuncionario - in progress
-
-
-//funcao para liberar memoria dos mallocs
+// Função para liberar memória dos mallocs
 void liberarMemorias(estruturafuncionario *funcionario, int numdefuncionarios) {
     for (int i = 0; i < numdefuncionarios; i++) {
         free(funcionario[i].nomefuncionario);
     }
 
     free(funcionario);
-} 
+}
 
-//funcao principal
+// Função para ler os dados do arquivo e armazená-los na estrutura
+void lerFuncionariosDoArquivo(estruturafuncionario *funcionario, int numdefuncionarios) {
+    FILE *arq_funcionarios = fopen("arq_funcionarios.txt", "r");
+    if (arq_funcionarios == NULL) {
+        printf("Erro ao abrir o arquivo de funcionarios.\n");
+        return;
+    }
 
-int main(){
-    
+    char buffer[200];
+    int i = 0;
+
+    while (fgets(buffer, sizeof(buffer), arq_funcionarios) != NULL && i < numdefuncionarios) {
+        if (strstr(buffer, "===== Informações dos funcionarios") != NULL) {
+            if (fgets(buffer, sizeof(buffer), arq_funcionarios)) {
+                char *nome = strstr(buffer, "Nome: ");
+                nome += strlen("Nome: ");
+                funcionario[i].nomefuncionario = malloc(30 * sizeof(char));
+                strcpy(funcionario[i].nomefuncionario, nome);
+                funcionario[i].nomefuncionario[strcspn(funcionario[i].nomefuncionario, "\n")] = 0;
+            }
+
+            if (fgets(buffer, sizeof(buffer), arq_funcionarios)) {
+                char *id = strstr(buffer, "ID: ");
+                id += strlen("ID: ");
+                sscanf(id, "%d", &funcionario[i].idfuncionario);
+            }
+
+            i++;
+        }
+    }
+
+    fclose(arq_funcionarios);
+}
+
+// Função principal
+int main() {
     int numdefuncionarios, numTipoDeOperacao;
 
-    printf("===== Menu de Operações =====\n \n Adicionar -> 1 \n Visualizar -> 2 \n alterar -> 3 \n Deletar -> 4 \n\nQual operação deseja realizar: ");
-      scanf("%d", &numTipoDeOperacao);
-        getchar();
-//chamada para adicionar funcionario
+    printf("===== Menu de Operações =====\n \n Adicionar -> 1 \n Visualizar -> 2 \n Alterar -> 3 \n Deletar -> 4 \n\nQual operação deseja realizar: ");
+    scanf("%d", &numTipoDeOperacao);
+    getchar();
+
     if (numTipoDeOperacao == 1) {
         printf("\nQuantos funcionarios você deseja adicionar: \n");
         scanf("%d", &numdefuncionarios);
@@ -172,9 +203,7 @@ int main(){
         adicionarfuncionario(&funcionario, &numdefuncionarios);
         salvarfuncionario(funcionario, numdefuncionarios);
         liberarMemorias(funcionario, numdefuncionarios);
-    }
-//chamada para visualizar funcionarios   
-    else if(numTipoDeOperacao == 2){
+    } else if (numTipoDeOperacao == 2) {
         numdefuncionarios = contarfuncionariosNoArquivo();
         if (numdefuncionarios > 0) {
             estruturafuncionario *funcionario = alocarEstruturaFuncionario(numdefuncionarios);
@@ -182,34 +211,27 @@ int main(){
             liberarMemorias(funcionario, numdefuncionarios);
         } else {
             printf("Não há veículos registrados para visualizar.\n");
-    }
-//chamada para alterar dados dos funcionarios    
-    }
-    else if(numTipoDeOperacao == 3){
-         numdefuncionarios = contarfuncionariosNoArquivo();
+        }
+    } else if (numTipoDeOperacao == 3) {
+    numdefuncionarios = contarfuncionariosNoArquivo();
     if (numdefuncionarios > 0) {
         estruturafuncionario *funcionario = alocarEstruturaFuncionario(numdefuncionarios);
-        visualizarfuncionario(funcionario, numdefuncionarios); // Opcional: Mostrar antes de alterar
+        lerFuncionariosDoArquivo(funcionario, numdefuncionarios); // Ler do arquivo
         alterarFuncionario(funcionario, numdefuncionarios);
+        salvarfuncionario(funcionario, numdefuncionarios);
         liberarMemorias(funcionario, numdefuncionarios);
     } else {
         printf("Não há funcionários registrados para alterar.\n");
     }
-    }
-//chamada para deletar funcionario    
-    else if(numTipoDeOperacao == 4){
-          numdefuncionarios = contarfuncionariosNoArquivo();
-          estruturafuncionario *funcionario = alocarEstruturaFuncionario(numdefuncionarios);
-          visualizarfuncionario(funcionario, numdefuncionarios);
-          liberarMemorias(funcionario, numdefuncionarios);
-          
-         // deletarFuncionario();
-    }
-    
-    else {
+}
+     else if (numTipoDeOperacao == 4) {
+        numdefuncionarios = contarfuncionariosNoArquivo();
+        estruturafuncionario *funcionario = alocarEstruturaFuncionario(numdefuncionarios);
+        visualizarfuncionario(funcionario, numdefuncionarios);
+        liberarMemorias(funcionario, numdefuncionarios);
+    } else {
         printf("Erro ao selecionar a operação\n");
     }
-    
-return 0;
 
+    return 0;
 }
